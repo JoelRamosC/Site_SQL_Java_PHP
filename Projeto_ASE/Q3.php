6330e3da3ee79e662b9c18672bb31daaae25b374
Joel Ramos <96001289+JoelRamosC@users.noreply.github.com>
2022-01-09 21:14:39 -0300

    Add files via upload

---
+++ Projeto_ASE/Q3.php
@@ -0,0 +1,76 @@
+
+<?php
+    include_once('conexao.php');
+    session_start();
+    $vetor_resposta = $_SESSION['vetor_resposta_acumulado'];
+?>
+
+<?php
+    mysqli_query($ligar,"SET NAMES 'utf8'");
+    mysqli_query($ligar,'SET character_set_connection=utf8');
+    mysqli_query($ligar,'SET character_set_client=utf8');
+    mysqli_query($ligar,'SET character_set_results=utf8');
+?>
+
+<!DOCTYPE html>
+<html lang="PT">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acesso</title>
+</head>
+
+<style>
+    .center {
+    margin: 0;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    -ms-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+    }
+</style>
+<body>
+    <td>
+    <div class="center">
+    <center>
+    <img src="WiseMachines.gif" width="283" height="60">
+    <h2> <p style="font-family:open sans;"> Analizador de Saúde Emocional </p></h2>
+    
+    <form style="text-align:center" 
+        action="http://localhost/wise_machines/analizador_de_saude_emocional/Q4.php" method="post">
+    <fieldset>
+        <legend> <b><p style="font-size:110%;font-family:open sans;" > 
+            <font  color="black" > 3/14 - Grau da sua percepção pessoal de prazer acumulado nos últimos 3 dias.</font> <br>
+            <font  color="darkorange"   > Diversão - Atividade Sexual -  Distração - Gastronomia - Viagem  </font> <br>
+            <font  color="black" > Grau 7 = Prazer Máximo <br> Grau 1 = Prazer Mínimo </font> </p></b></legend>
+        <input type="radio" name="valor_Q3" value="7" required> <b><font  color="black" style="font-family:open sans;" >  7  </font> </b> <br><br>
+        <input type="radio" name="valor_Q3" value="6" required> <b><font  color="black" style="font-family:open sans;" >  6  </font> </b> <br><br>
+        <input type="radio" name="valor_Q3" value="5" required> <b><font  color="black" style="font-family:open sans;" >  5  </font> </b> <br><br>
+        <input type="radio" name="valor_Q3" value="4" required> <b><font  color="black" style="font-family:open sans;" >  4  </font> </b> <br><br>
+        <input type="radio" name="valor_Q3" value="3" required> <b><font  color="black" style="font-family:open sans;" >  3  </font> </b> <br><br>
+        <input type="radio" name="valor_Q3" value="2" required> <b><font  color="black" style="font-family:open sans;" >  2  </font> </b> <br><br>
+        <input type="radio" name="valor_Q3" value="1" required> <b><font  color="black" style="font-family:open sans;" >  1  </font> </b> <br><br>
+        <br>
+        <input type="submit" value="Próximo">
+    </fieldset>
+    </form></code>
+
+    <?php
+        if(isset($_POST["valor_Q3"])) {
+            $vetor_resposta[3] = $_POST["valor_Q3"];
+            print_r ($vetor_resposta);
+        }
+        else {
+            $vetor_resposta[2] = $_POST["valor_Q2"];
+            $_SESSION['vetor_resposta_acumulado'] = $vetor_resposta;
+            //print_r ($vetor_resposta);
+        }
+    ?>
+    
+</body>
+</html>                             
+
+
+   
---
+++ Projeto_ASE/Q5.php
@@ -0,0 +1,76 @@
+
+<?php
+    include_once('conexao.php');
+    session_start();
+    $vetor_resposta = $_SESSION['vetor_resposta_acumulado'];
+?>
+
+<?php
+    mysqli_query($ligar,"SET NAMES 'utf8'");
+    mysqli_query($ligar,'SET character_set_connection=utf8');
+    mysqli_query($ligar,'SET character_set_client=utf8');
+    mysqli_query($ligar,'SET character_set_results=utf8');
+?>
+
+<!DOCTYPE html>
+<html lang="PT">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acesso</title>
+</head>
+
+<style>
+    .center {
+    margin: 0;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    -ms-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+    }
+</style>
+<body>
+    <td>
+    <div class="center">
+    <center>
+    <img src="WiseMachines.gif" width="283" height="60">
+    <h2> <p style="font-family:open sans;"> Analizador de Saúde Emocional </p></h2>
+    
+    <form style="text-align:center" 
+        action="http://localhost/wise_machines/analizador_de_saude_emocional/Q6.php" method="post">
+    <fieldset>
+        <legend> <b><p style="font-size:110%;font-family:open sans;" > 
+            <font  color="black" > 5/14 - Grau de qualidade da sua alimentação acumulada nos últimos 3 dias.  </font> <br>
+            <font  color="gold"   > Nutrição - Hidratação - Digestão - Exercício - Disposição  </font> <br>
+            <font  color="black" > Grau 7 = Qualidade Máxima <br> Grau 1 = Qualidade Mínima </font> </p></b></legend>
+        <input type="radio" name="valor_Q5" value="7" required> <b><font  color="black" style="font-family:open sans;" >  7  </font> </b> <br><br>
+        <input type="radio" name="valor_Q5" value="6" required> <b><font  color="black" style="font-family:open sans;" >  6  </font> </b> <br><br>
+        <input type="radio" name="valor_Q5" value="5" required> <b><font  color="black" style="font-family:open sans;" >  5  </font> </b> <br><br>
+        <input type="radio" name="valor_Q5" value="4" required> <b><font  color="black" style="font-family:open sans;" >  4  </font> </b> <br><br>
+        <input type="radio" name="valor_Q5" value="3" required> <b><font  color="black" style="font-family:open sans;" >  3  </font> </b> <br><br>
+        <input type="radio" name="valor_Q5" value="2" required> <b><font  color="black" style="font-family:open sans;" >  2  </font> </b> <br><br>
+        <input type="radio" name="valor_Q5" value="1" required> <b><font  color="black" style="font-family:open sans;" >  1  </font> </b> <br><br>
+        <br>
+        <input type="submit" value="Próximo">
+    </fieldset>
+    </form></code>
+
+    <?php
+        if(isset($_POST["valor_Q5"])) {
+            $vetor_resposta[5] = $_POST["valor_Q5"];
+            print_r ($vetor_resposta);
+        }
+        else {
+            $vetor_resposta[4] = $_POST["valor_Q4"];
+            $_SESSION['vetor_resposta_acumulado'] = $vetor_resposta;
+            //print_r ($vetor_resposta);
+        }
+    ?>
+    
+</body>
+</html>                             
+
+
+   
